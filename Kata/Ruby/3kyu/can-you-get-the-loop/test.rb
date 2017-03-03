54bad41298a3e5aa55417ed37fa12984eda306a5
BenJohnCarson <benjohncarson@gmail.com>
2017-03-03 12:29:18 +0000

    03/03/2017, 12:29:18

--- Kata/Ruby/3kyu/can-you-get-the-loop/test.rb
+++ Kata/Ruby/3kyu/can-you-get-the-loop/test.rb
@@ -1,7 +1,7 @@
 #use this if you want to create your code on you computer the class of the node is in the description
 def create_chain_for_test(tail_size, loop_size)
   prev_node = start = Node.new
-  start.next = start
+  
   return start if loop_size == 1
   (1..tail_size).each do |i|
     prev_node.next = Node.new
