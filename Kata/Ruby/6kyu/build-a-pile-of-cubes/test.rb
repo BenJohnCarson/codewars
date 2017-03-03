e0ff049479632855e7992cccb90fabd93673211c
BenJohnCarson <benjohncarson@gmail.com>
2017-03-03 10:30:02 +0000

    03/03/2017, 10:30:02

---
+++ Kata/Ruby/6kyu/build-a-pile-of-cubes/test.rb
@@ -0,0 +1,4 @@
+Test.assert_equals(find_nb(4183059834009), 2022)
+Test.assert_equals(find_nb(24723578342962), -1)
+Test.assert_equals(find_nb(135440716410000), 4824)
+Test.assert_equals(find_nb(40539911473216), 3568)
